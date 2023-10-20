ee3b410963a6f3858fe5f0f9ad6d6e53d52a2f7d
mohongen <496585582@qq.com>
2023-10-20 10:42:30 +0800

    chore: 去除不必要的逻辑

--- plugins/my-plugins.client.ts
+++ plugins/my-plugins.client.ts
@@ -12,7 +12,7 @@ export default defineNuxtPlugin(async (nuxtApp) => {
       window.Vue = vue; // ? 提供element-plus初始化, 若单独走cdn会导致element-plus初始化时版本和注水之前版本不统一
       // nuxtApp.vueApp.use(Elm);
       // @ts-ignore
-      console.info(Elm.version, 'version')
+      // console.info(Elm.version, 'version')
       // @ts-ignore
       // nuxtApp.vueApp.provide(Elm.ID_INJECTION_KEY, {
       //     prefix: Math.floor(Math.random() * 10000),
